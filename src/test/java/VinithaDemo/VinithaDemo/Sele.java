3171a91ef45cef385612048f0a26dcb9b39b393c
kesavasubash <kesavasubash@gmail.com>
2020-08-14 09:50:45 +0530

    updated

---
+++ src/test/java/VinithaDemo/VinithaDemo/Sele.java
@@ -0,0 +1,15 @@
+package VinithaDemo.VinithaDemo;
+
+public class Sele {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+
+
+	}
+
+}
